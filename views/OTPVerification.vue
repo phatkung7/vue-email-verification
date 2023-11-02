6e6e9a867321c5e5ab6dd9cc8f95a6ae2e53e5fe
Sataphat Denduang <phatkung.kizz@gmail.com>
2023-11-02 21:09:50 +0700

    Update OTPVerification.vue

--- views/OTPVerification.vue
+++ views/OTPVerification.vue
@@ -5,18 +5,37 @@
         <div class="card">
           <div class="card-body">
             <h2 class="card-title text-center mb-4">OTP Verification</h2>
-            <form @submit.prevent="submitOTP">
-              <div class="mb-3">
-                <label for="otp" class="form-label">Enter OTP</label>
-                <input
-                  type="text"
-                  class="form-control"
-                  id="otp"
-                  v-model="otp"
-                  required
-                />
+            <form @submit.prevent="submitOTP" ref="otpForm">
+              <div class="mb-3 text-center">
+                <label for="otp" class="form-label"
+                  >กรุณาใส่รหัสยืนยัน (OTP Code)</label
+                >
               </div>
-              <button type="submit" class="btn btn-primary btn-block">
+              <div class="d-flex justify-content-center align-items-center">
+                <div
+                  v-for="(digit, index) in otpArray"
+                  :key="index"
+                  class="mx-2"
+                >
+                  <input
+                    ref="otpInput"
+                    v-model="otpArray[index]"
+                    type="text"
+                    class="form-control text-center"
+                    required
+                    pattern="\d"
+                    maxlength="1"
+                    @input="handleInput(index)"
+                    @keydown="handleKeydown(index, $event)"
+                    :tabindex="index"
+                  />
+                </div>
+              </div>
+              <button
+                type="submit"
+                class="btn btn-primary btn-block mt-4"
+                :disabled="isInputIncomplete"
+              >
                 Verify OTP
               </button>
               <div class="text-center text-danger mt-3">
@@ -33,27 +52,60 @@
 <script>
 import liff from "@line/liff";
 import axios from "axios";
-//import Swal from "sweetalert2";
-//const LINE_DDC_HOOK = process.env.LINE_DDC_HOOK
-const VUE_APP_API_KEY = process.env.VUE_APP_API_KEY;
+import Swal from "sweetalert2";
+const LINE_HOOK_REGISTER_OTP = process.env.VUE_APP_LINE_HOOK_REGISTER_OTP;
+const DDC_API = process.env.VUE_APP_DDC_API;
+const DDC_API_KEY = process.env.VUE_APP_API_KEY;
+const LIFF_ID = process.env.VUE_APP_LIFF_ID;
 export default {
   props: ["email"],
   data() {
     return {
       otp: "",
       termsText: "",
+      otpArray: ["", "", "", "", "", ""],
     };
   },
+  computed: {
+    isInputIncomplete() {
+      return this.otpArray.some((digit) => digit === "");
+    },
+  },
   beforeCreate() {
-    liff.init({ liffId: process.env.VUE_APP_LIFF_ID }, function () {});
+    liff.init({ liffId: LIFF_ID }, function () {});
   },
   async mounted() {
-    await this.checkLiffLogin();
-    // Access the email parameter from the route
-    //const email = this.$route.params.email;
-    //console.log("Email received in /otp:", email);
+    //await this.checkLiffLogin();
+    this.$refs.otpInput[0].focus();
+  },
+  watch: {
+    otpArray: {
+      handler() {
+        if (!this.isInputIncomplete && this.$refs.otpForm) {
+          // Auto-submit the form when OTP is complete and form reference is available
+          this.submitOTP();
+        }
+      },
+      deep: true,
+    },
   },
   methods: {
+    handleInput(index) {
+      // Sanitize input to allow only integers
+      this.otpArray[index] = this.otpArray[index].replace(/\D/g, "");
+      // Handle input logic if needed
+      console.log(index);
+      if (index < 5 && this.otpArray[index] !== "") {
+        // Move to the next input box
+        this.$refs.otpInput[index + 1].focus();
+      }
+    },
+    handleKeydown(index, event) {
+      // Handle backspace key to maintain cursor position
+      if (event.key === "Backspace" && index > 0 && !this.otpArray[index]) {
+        this.$refs.otpInput[index - 1].focus();
+      }
+    },
     async checkLiffLogin() {
       await liff.ready.then(async () => {
         if (!liff.isLoggedIn()) {
@@ -63,18 +115,18 @@ export default {
     },
     async SubmitUser(userData) {
       const idToken = await liff.getIDToken();
-      console.log('-----idToken----');
+      console.log("-----idToken----");
       console.log(idToken);
-      console.log('---------');
-      console.log('-----userData----');
+      console.log("---------");
+      console.log("-----userData----");
       console.log(userData.email);
       console.log(userData.otp);
-      console.log('---------');
+      console.log("---------");
       try {
         // Make an API call using Axios
         const response = await axios({
           method: "post",
-          url: "https://e0d5-203-156-15-252.ngrok-free.app/webhook-ddc-helpdesk-51c5f/asia-northeast1/register-auth",
+          url: LINE_HOOK_REGISTER_OTP,
           headers: {
             Authorization: idToken,
           },
@@ -107,39 +159,51 @@ export default {
       }
     },
     async submitOTP() {
+      const enteredOTP = this.otpArray.join("");
       try {
         await axios
           .post(
-            "https://api.ddc.moph.go.th/api-helpdesk/verify-otp",
+            DDC_API + "verify-otp",
             {
               email: this.$route.params.email,
-              otp: this.otp,
+              otp: enteredOTP,
             },
             {
               headers: {
-                "x-api-key": VUE_APP_API_KEY,
+                "x-api-key": DDC_API_KEY,
                 "Content-Type": "application/json",
               },
             }
           )
           .then(async (response) => {
             if (response.data.status == "success") {
-              console.log("Log from DDC API:", response.data.data[0].email);
-              // Post Data To SubmitUser
-              try {
-                // Call the SubmitUser method with Axios
-                const requestData = {
-                  email: response.data.data[0].email,
-                  otp: response.data.data[0].otp_code,
-                };
-                console.log(requestData);
-                await this.SubmitUser(requestData);
-                console.log("SubmitUser success");
-              } catch (error) {
-                console.error("SubmitUser error:", error);
+              //console.log("Log from DDC API:", response.data.data[0].email);
+              // Check if response.data.data exists and has at least one element
+              if (response.data.data && response.data.data.length > 0) {
+                // Post Data To SubmitUser
+                try {
+                  // Call the SubmitUser method with Axios
+                  const requestData = {
+                    email: response.data.data[0].email,
+                    otp: response.data.data[0].otp_code,
+                  };
+                  //console.log(requestData);
+                  await this.SubmitUser(requestData);
+                  console.log("SubmitUser success");
+                } catch (error) {
+                  console.error("SubmitUser error:", error);
+                }
+              } else {
+                this.termsText = "No data received from DDC API";
               }
             } else {
-              this.termsText = "เกิดข้อผิดพลาด: " + response.data.message;
+              //this.termsText = "เกิดข้อผิดพลาด: " + response.data.message;
+              Swal.fire({
+                icon: "error",
+                title: "เกิดข้อผิดพลาด",
+                text: response.data.message,
+                confirmButtonText: "ตกลง",
+              });
             }
           });
       } catch (error) {
@@ -147,70 +211,6 @@ export default {
         console.error("Error posting data:", error);
         this.termsText = "เกิดข้อผิดพลาด: API Error";
       }
-
-      // const api_verify_otp =
-      //   "https://e0d5-203-156-15-252.ngrok-free.app/webhook-ddc-helpdesk-51c5f/asia-northeast1/register-auth";
-      // console.log("API URL:", api_verify_otp);
-      // await axios({
-      //   method: "post",
-      //   url: api_verify_otp,
-      //   headers: {
-      //     Authorization: idToken,
-      //   },
-      //   data: {
-      //     email: this.$route.params.email,
-      //     otp: this.otp,
-      //   },
-      // })
-      //   .then((response) => {
-      //     // handle success
-      //     console.log(response);
-      //     //alert("Message sent");
-      //     //liff.closeWindow();
-      //   })
-      //   .catch((error) => {
-      //     // handle errors
-      //     //alert("Error Message sent");
-      //     console.log(error);
-      //   });
-
-      //console.log(response_verify_user.data.status);
-      // Check if the status is success
-      //if (response_verify_otp.data.status === "success") {
-      // This is where you'd typically make an API request to your server
-      //console.log(`Verification code sent to ${this.email}`);
-      // Show SweetAlert success message
-      // Swal.fire({
-      //   icon: "success",
-      //   title: "สำเร็จ!",
-      //   text: "กรุณาตรวจสอบ Inbox อีเมล์ของท่าน",
-      //   confirmButtonText: "ตกลง",
-      // });
-      // Now, navigate to the OTP component
-      //this.$router.push('/otp');
-      //this.$router.push({ name: "OtpPage", params: { email: this.email } });
-      // } else {
-      //   // Handle the case where the API call was not successful
-      //   console.error("User not found or other error occurred");
-      //   this.termsText = "เกิดข้อผิดพลาด กรุณาลองอีกครั้ง.";
-      // }
-      // } catch (error) {
-      //   // Handle the case where the API call fails
-      //   console.error("Error posting data:", error);
-      //   this.termsText = "เกิดข้อผิดพลาดไม่พบอีเมล์ของท่านในระบบ.";
-      // }
-
-      //const idToken = await liff.getIDToken();
-      //   const getLiffemail = await liff.getDecodedIDToken().email;
-      //console.log("------ Start App Logs ----");
-      //console.log("idToken", idToken);
-      //   console.log("email", getLiffemail);
-      //console.log(`OTP ${this.$route.params.email} verified successfully`);
-      // Call your backend API to verify the OTP
-      // This is where you'd typically make an API request to your server
-      //console.log(`OTP ${this.otp} verified successfully`);
-      // Handle success or failure accordingly
-      //console.log("------ End App Logs ----");
     },
   },
 };
