e66e4406aea51a8d8ce339ab5916c9f3d7536f12
Sataphat Denduang <phatkung.kizz@gmail.com>
2023-11-10 10:42:58 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,4 +4,11 @@ import router from "../router";
 
 import "bootstrap/dist/css/bootstrap.css";
 
+// Your vConsole hide code
+if (process.env.VUE_APP_ENV === "production") {
+  if (window.vConsole) {
+    window.vConsole.hideSwitch();
+  }
+}
+
 createApp(App).use(router).mount("#app");
