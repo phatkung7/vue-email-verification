43cf8c593ef53e3b3e139ac6a55dec65f2b43fed
Sataphat Denduang <phatkung.kizz@gmail.com>
2024-01-09 13:04:38 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "../router";
-import VConsole from "vconsole";
+//import VConsole from "vconsole";
 import "bootstrap/dist/css/bootstrap.css";
 // Initialize vConsole
-const vConsole = new VConsole();
+//const vConsole = new VConsole();
 
 // Conditionally initialize vConsole in development
-if (process.env.VUE_APP_ENV === "production" && vConsole) {
-  vConsole.hideSwitch();
-}
+// if (process.env.VUE_APP_ENV === "production" && vConsole) {
+//   vConsole.hideSwitch();
+// }
 
 createApp(App).use(router).mount("#app");
